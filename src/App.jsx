2d523f1c59c443ddab65ffdd16ffba88bc28c75e
Den <103452827+LourdenB15@users.noreply.github.com>
2025-08-03 18:07:25 +0800

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import "./styles/reset.css";
 import "./styles/styles.css";
 import Header from './components/Header';
 import Profile from './components/Profile';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Profile />
-      {/* 
-      <Footer /> */}
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../styles/reset.css";
+import "../styles/styles.css";
+
+function Footer() {
+    return (
+       <footer>
+        <p>© 2025 Den. All rights reserved.</p>
+       </footer>
+    )
+}
+
+export default Footer;
